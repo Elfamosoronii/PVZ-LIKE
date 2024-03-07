d09cf6bcb8a67a3677e5dec61f9543d3ee478aaf
Roni POGGIOLI <roni.poggioli@brassart.fr>
2024-03-07 15:53:38 +0100

    hihi

--- Assets/Scripts/TilemapData.cs
+++ Assets/Scripts/TilemapData.cs
@@ -4,6 +4,23 @@ using UnityEngine;
  [System.Serializable]
 public class TilemapData
 {
-    public bool IsValid = true;
+    public bool IsValid
+    {
+        get
+        {
+            return hasTile && building is null;
+        }
+
+    }
+
     private int layerIndex = -1;
+
+
+    private bool hasTile = false;
+    private GameObject building = null; 
+
+    public TilemapData(bool _hasTile)
+    {
+        hasTile = _hasTile;
+    }
 }
